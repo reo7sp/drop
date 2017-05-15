301d11e75baf2c7d7d20a78f55a5345171f1c2d9
Oleg Morozenkov <reo7sp@users.noreply.github.com>
2017-05-15 05:07:29 +0300

    Basic http server

---
+++ models.go
@@ -0,0 +1,5 @@
+package main
+
+type Drop struct {
+	Text string
+}
--- web.go
+++ web.go
@@ -1,13 +1,28 @@
 package main
 
-import "gopkg.in/gin-gonic/gin.v1"
+import (
+	"gopkg.in/gin-gonic/gin.v1"
+	"net/http"
+)
 
 func InitWeb() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+	r.LoadHTMLGlob("templates/*")
+	r.GET("/", func(c *gin.Context) {
+		// TODO: fetch drops
+
+		c.HTML(http.StatusOK, "index.html.tmpl", []Drop{{"aaaa"}, {"bbbb"}})
+	})
+	r.POST("/", func(c *gin.Context) {
+		text := c.PostForm("text")
+		if text == "" {
+			c.String(http.StatusBadRequest, "empty text")
+			return
+		}
+
+		// TODO: save drop
+
+		c.String(http.StatusCreated, "OK")
 	})
 	r.Run()
 }
